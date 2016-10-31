6bad6aed269ea144bb2bcf9b14ee54ed0dedf69a
veenaw <vwagle2@gmail.com>
2016-10-31 16:32:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/threadingviewpost/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.android.threadingviewpost;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private Bitmap mBitmap;
+    private ImageView mImageView;
+    private int mDelay = 5000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.imageView);
+
+        final Button button = (Button) findViewById(R.id.loadButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadIcon();
+            }
+        });
+
+        final Button otherButton = (Button) findViewById(R.id.otherButton);
+        otherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "I'm Working",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadIcon() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(mDelay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                mBitmap = BitmapFactory.decodeResource(getResources(),
+                        R.drawable.painter);
+                mImageView.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        mImageView.setImageBitmap(mBitmap);
+                    }
+                });
+            }
+        }).start();
+    }
+}
